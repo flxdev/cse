f15877f6378e13b17720d3f12cf1333025522457
kotvasili <kot_vasili@mail.ru>
2017-12-05 18:21:55 +0300

    critical and some shit

--- gulp/tasks/critical.js
+++ gulp/tasks/critical.js
@@ -5,17 +5,13 @@ var critical = require('critical').stream;
 import gutil from 'gulp-util';
 
 gulp.task('critical', function(){ return gulp.src([config.dest.html + "/*.html"])
-  .on('end', function(err) { gutil.log(config.dest.html,config.dest.css) })
   .pipe(critical({
-    base: config.dest.html  + "/",
+    base: config.dest.html,
     inline: true,
     minify: true,
-    extract: true,
     css: [config.dest.css + '/app.css'],
   }))
-    .on('end', function(err) { gutil.log(config.dest.html,config.dest.css) })
   .pipe(gulp.dest(config.dest.html +''))
-
  });
 
 
